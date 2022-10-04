005c263af2e1741bfff2056407c6d06e615aa221
Praveen Yaragatti <57564746+YaragattiPraveen@users.noreply.github.com>
2022-10-04 23:44:04 +0530

    InitialCommit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,71 @@
-import logo from './logo.svg';
 import './App.css';
+import React, { useEffect } from 'react';
+import { useState } from 'react';
+import Visibility from './components/Visibility';
+import axios from 'axios';
 
 function App() {
+  const [visibility, setvisibility] = useState('none')
+  const [search, setsearch] = useState("")
+  const [cocktailList, setcocktailList] = useState([])
+
+  useEffect(()=>{
+    fetchCocktailName() // eslint-disable-next-line
+  },[])
+
+
+  const fetchCocktailName = async () => {
+    const url = `https://www.thecocktaildb.com/api/json/v1/1/search.php?s=${search}`
+    try {
+      const data = await axios.get(url);
+      console.log(data.data.drinks)
+      setcocktailList(data.data.drinks)
+    } catch (error) {
+      console.log(error)
+    }
+  }
+
+  const setDisplays = () => {
+    setvisibility('unset')
+  }
+  const removeDisplay = () => {
+    setvisibility('none')
+  }
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div className="App">
+        <form onSubmit={(e) => e.preventDefault()}>
+          <h1>Find Your Favourite Cocktail</h1>
+          <div className='search_box'>
+            <input type='text' placeholder='Search with cocktail name...' onChange={(e) => setsearch(e.target.value)} />
+            <button onClick={fetchCocktailName}>Search</button>
+          </div>
+        </form>
+        <div className='card_container'>
+          {
+            cocktailList.map((val, index) => {
+              return (
+                <div className='details_card'>
+                  <img src={val.strDrinkThumb} alt='cocktail' />
+                  <h4>{val.strDrink}</h4>
+                  <div className='more_details'>
+                    <p>Alcoholic : <span>{val.strAlcoholic}</span></p>
+                    <p>Category : <span>{val.strCategory}</span></p>
+                  </div>
+                  <button className='btn' onClick={setDisplays}>More Info..</button>
+                  <Visibility visibility={visibility} hide={removeDisplay} 
+                    title={val.strDrink} description={val.strInstructions}
+                  />
+                </div>
+              )
+            })
+          }
+        </div>
+      </div>
+
+    </>
   );
 }
 
---
+++ src/components/Visibility.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+
+function Visibility(props) {
+    return (
+        <>
+            <div style={{ display: `${props.visibility}` }} className='center_container'>
+                <div className='card'>
+                    <h3>{props.title}</h3>
+                    <button onClick={props.hide}>X</button>
+                    <p>{props.description}</p>
+                </div>
+            </div>
+        </>
+    )
+}
+
+export default Visibility;
